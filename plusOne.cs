0c679257de29174dabfc1f675197c6c0a0301f69
Armando Yanez- Laptop <ayanez90@live.com>
2021-11-08 20:00:48 -0800

    beginning stages of testing getting values from list and listing them as their number value

--- plusOne.cs
+++ plusOne.cs
@@ -6,7 +6,16 @@ namespace PlusOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            int[] testNums = new int[]{1,2,3,4,5,6,7,8,9};
+            int currentValues = 1;
+
+            int ones = testNums[testNums.Length - 1] * currentValues;
+            currentValues *= 10;
+            int tens = testNums[testNums.Length - 2] * currentValues;
+            currentValues *= 10;
+
+            Console.WriteLine($"currentValues: {currentValues}\n ones: {ones}\n tens:{tens}");
+
         }
     }
 }
